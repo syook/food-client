a18842aedf26c4ffe23fdf477d8c64d316650373
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-12-07 14:22:05 +0530

    formSection stylings added

--- src/commonComponents/navbar/index.js
+++ src/commonComponents/navbar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
 
 function Navbar() {
@@ -8,16 +8,16 @@ function Navbar() {
       <Grid divided relaxed={true} doubling={true}>
         <Grid.Row stretched={true} verticalAlign="middle">
           <Grid.Column className="nav-item" width={2}>
-            <Link to='/dashboard'>Logo</Link>
+            <Link to="/dashboard">Logo</Link>
           </Grid.Column>
           <Grid.Column className="nav-item" width={4}>
-           <Link to='/users'> Users</Link>
+            <Link to="/users"> Users</Link>
           </Grid.Column>
           <Grid.Column className="nav-item" width={5}>
-             <Link to='/fooditems'> Food Items</Link>
+            <Link to="/fooditems"> Food Items</Link>
           </Grid.Column>
           <Grid.Column className="nav-item" width={5}>
-            <Link to='/dailymenu'> Daily Menu</Link>
+            <Link to="/dailymenu"> Daily Menu</Link>
           </Grid.Column>
         </Grid.Row>
       </Grid>
--- src/features/users/containers/form.js
+++ src/features/users/containers/form.js
@@ -1,55 +1,77 @@
-import React from 'react'
-import { Form ,Button} from 'semantic-ui-react'
+import React from "react";
+import { Form, Button } from "semantic-ui-react";
 
+class UserForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      mobile: "",
+      defaultChapati: ""
+    };
+  }
 
-class UserForm extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={
-            name:'',
-            email:'',
-            mobile:'',
-            defaultChapati:''
-        }
-    }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
 
-    handleChange=(e)=>{
-        this.setState({[e.target.name]:e.target.value})
+  render() {
+    return (
+      <div className="formSection">
+        <Form>
+          <Form.Field>
+            <label> Name</label>
+            <input
+              name="name"
+              placeholder="First Name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Email</label>
+            <input
+              name="email"
+              placeholder="Email"
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Mobile</label>
+            <input
+              name="mobile"
+              placeholder="Mobile"
+              type="number"
+              value={this.state.mobile}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Default chapati</label>
+            <input
+              name="defaultChapati"
+              placeholder="count"
+              type="number"
+              value={this.state.defaultChapati}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
 
-    }
-    handleSubmit=(e)=>{
-        e.preventDefault()
-        console.log(this.state)
-
-    }
-
-
-render(){
-    return(
-            <div>
-            <Form>
-                <Form.Field>
-                <label> Name</label>
-                <input name= 'name' placeholder='First Name' type='text' value={this.state.name} onChange={this.handleChange} />
-                </Form.Field>
-                <Form.Field>
-                <label>Email</label>
-                <input name= 'email' placeholder='Email' type='email' value={this.state.email} onChange={this.handleChange} />
-                </Form.Field>
-                <Form.Field>
-                <label>Mobile</label>
-                <input name= 'mobile' placeholder='Mobile' type='number' value={this.state.mobile} onChange={this.handleChange}/>
-                </Form.Field>
-                <Form.Field>
-                <label>Default chapati</label>
-                <input name= 'defaultChapati' placeholder='count' type='number' value={this.state.defaultChapati} onChange={this.handleChange} />
-                </Form.Field>
-                
-                <Button type='submit' onClick={this.handleSubmit}>Submit</Button>
-            </Form>
-            </div>
-        )
-    }
+          <Button type="submit" onClick={this.handleSubmit}>
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
 
-export default UserForm
+export default UserForm;
