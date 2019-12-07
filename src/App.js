675142b1aeb37b70571a16c9567b27ac71491843
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-12-07 12:18:45 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Navbar from "./commonComponents/navbar";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar />
     </div>
   );
 }
