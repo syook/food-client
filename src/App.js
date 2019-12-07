8f23d5756778368b075a3a947588eb054b82445f
harsh <harsh@localhost.localdomain>
2019-12-07 12:18:35 +0530

    users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Users from './features/users/containers/form'
+
+import 'semantic-ui-css/semantic.min.css'
+
 function App() {
   return (
     <div className="App">
-    
+    <Users/>
+
     </div>
   );
 }
---
+++ src/features/users/containers/form.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Form ,Button} from 'semantic-ui-react'
+
+
+class Users extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            name:'',
+            email:'',
+            mobile:'',
+            defaultChapati:''
+        }
+    }
+
+    handleChange=(e)=>{
+        this.setState({[e.target.name]:e.target.value})
+
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log(this.state)
+
+    }
+
+
+render(){
+    return(
+            <div>
+            <Form>
+                <Form.Field>
+                <label> Name</label>
+                <input name= 'name' placeholder='First Name' type='text' value={this.state.name} onChange={this.handleChange} />
+                </Form.Field>
+                <Form.Field>
+                <label>Email</label>
+                <input name= 'email' placeholder='Email' type='email' value={this.state.email} onChange={this.handleChange} />
+                </Form.Field>
+                <Form.Field>
+                <label>Mobile</label>
+                <input name= 'mobile' placeholder='Mobile' type='number' value={this.state.mobile} onChange={this.handleChange}/>
+                </Form.Field>
+                <Form.Field>
+                <label>Default chapati</label>
+                <input name= 'defaultChapati' placeholder='count' type='number' value={this.state.defaultChapati} onChange={this.handleChange} />
+                </Form.Field>
+                
+                <Button type='submit' onClick={this.handleSubmit}>Submit</Button>
+            </Form>
+            </div>
+        )
+    }
+}
+
+export default Users
