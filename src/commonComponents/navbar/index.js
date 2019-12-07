5e98aa47a0096e26f0a0ef2021b9332c9e36db0c
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-12-07 15:02:26 +0530

    menu added for navbar

--- src/commonComponents/navbar/index.js
+++ src/commonComponents/navbar/index.js
@@ -1,26 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Grid } from "semantic-ui-react";
+import { Menu } from "semantic-ui-react";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <Grid divided relaxed={true} doubling={true}>
-        <Grid.Row stretched={true} verticalAlign="middle">
-          <Grid.Column className="nav-item" width={2}>
-            <Link to="/dashboard">Logo</Link>
-          </Grid.Column>
-          <Grid.Column className="nav-item" width={4}>
-            <Link to="/users"> Users</Link>
-          </Grid.Column>
-          <Grid.Column className="nav-item" width={5}>
-            <Link to="/fooditems"> Food Items</Link>
-          </Grid.Column>
-          <Grid.Column className="nav-item" width={5}>
-            <Link to="/dailymenu"> Daily Menu</Link>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <Menu>
+        <Menu.Item name="logo">
+          <Link to="/dashboard">Logo</Link>
+        </Menu.Item>
+        <Menu.Item name="users">
+          <Link to="/users"> Users</Link>
+        </Menu.Item>
+        <Menu.Item name="foodItems">
+          <Link to="/fooditems"> Food Items</Link>
+        </Menu.Item>
+        <Menu.Item name="dailyMenu">
+          <Link to="/dailymenu"> Daily Menu</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 }
