867ac0aae9c9b2a9cde8a21f80a17543fff094b0
Mayank Khajanchi <mayankkhajnchi@gmail.com>
2019-12-07 17:11:14 +0530

    card instead of table

--- src/features/foodItems/containers/table.js
+++ src/features/foodItems/containers/table.js
@@ -1,38 +1,32 @@
 import React from "react";
-import SyookTable from "@syook/react-tabulous";
+import { Icon } from "semantic-ui-react";
 
 const FoodItemTable = props => {
-  const columnDefs = () => [
+  const userDetails = [
     {
-      headerName: "Name",
-      field: "fooditem_name",
-      type: "String",
-      cell: foodItem => foodItem.name,
-      isSortable: true,
-      isSearchable: true,
-      isFilterable: false
+      name: "Shyam",
+      email: "shyam@syook.com",
+      phone: 9876543210,
+      defaultChapati: 2
     },
     {
-      headerName: "Type",
-      field: "fooditem_type",
-      type: "String",
-      cell: foodItem => foodItem.type,
-      isSortable: true,
-      isSearchable: true,
-      isFilterable: false
+      name: "Ram",
+      email: "ram@syook.com",
+      phone: 9876543210,
+      defaultChapati: 2
     }
   ];
   return (
-    <SyookTable
-      // actionOnHover
-      columnDefs={columnDefs({ ...props })}
-      key={props.currentData}
-      data={props.currentData || []}
-      mandatoryFields={["Name", "Type"]}
-      tableName={"Food Items"}
-    >
-      {props.children}
-    </SyookTable>
+    <div>
+      <div className="full-width formSection">
+        <div className="flex-start full-width pt-20 info">
+          <label>
+            <Icon name="calendar alternate outline" size="med" />:{" "}
+            <span>07/12/2019</span>
+          </label>
+        </div>
+      </div>
+    </div>
   );
 };
 export default FoodItemTable;
--- src/features/users/containers/table.js
+++ src/features/users/containers/table.js
@@ -1,57 +1,77 @@
 import React from "react";
 import SyookTable from "@syook/react-tabulous";
+import { Icon, Card, Button } from "semantic-ui-react";
 
 const UserTable = props => {
-  const columnDefs = () => [
+  const userDetails = [
     {
-      headerName: "Name",
-      field: "user_name",
-      type: "String",
-      cell: user => user.name,
-      isSortable: true,
-      isSearchable: true,
-      isFilterable: false
+      name: "Shyam",
+      email: "shyam@syook.com",
+      phone: 9876543210,
+      defaultChapati: 2,
+      defaultRicePortion: "Full"
     },
     {
-      headerName: "Email",
-      field: "user_email",
-      type: "Email",
-      cell: user => user.email,
-      isSortable: true,
-      isSearchable: true,
-      isFilterable: false
-    },
-    {
-      headerName: "Mobile",
-      field: "user_email",
-      type: "Number",
-      cell: user => user.mobile,
-      isSortable: true,
-      isSearchable: true,
-      isFilterable: false
-    },
-    {
-      headerName: "Default chapati",
-      field: "user_default_chapati",
-      type: "Number",
-      cell: user => user.defaultChapati,
-      isSortable: true,
-      isSearchable: true,
-      isFilterable: false
-      
+      name: "Ram",
+      email: "ram@syook.com",
+      phone: 9876543210,
+      defaultChapati: 1,
+      defaultRicePortion: "Double"
     }
-
   ];
   return (
-    <SyookTable
-      // actionOnHover
-      columnDefs={columnDefs({ ...props })}
-      data={props.currentData || []}
-      mandatoryFields={["Name", "Email","Mobile"]}
-      tableName={"Users"}
-    >
-      {props.children}
-    </SyookTable>
+    <div>
+      {/* <div className="full-width formSection">
+        <div className="flex-start full-width pt-20 info">
+          <label>
+            <Icon name="calendar alternate outline" size="med" />:{" "}
+            <span>07/12/2019</span>
+          </label>
+        </div>
+      </div> */}
+      {userDetails.map(item => {
+        return (
+          <Card>
+            <Card.Content>
+              <Card.Header>{item.name}</Card.Header>
+              <Card.Meta>{item.email}</Card.Meta>
+              <Card.Meta>{item.phone}</Card.Meta>
+              <Card.Description
+                style={{ display: "flex", justifyContent: "space-between" }}
+              >
+                <span>
+                  Default Chapati :<strong> {item.defaultChapati}</strong>
+                </span>
+                <span>|</span>
+                <span>
+                  Default Rice Portion :
+                  <strong> {item.defaultRicePortion}</strong>
+                </span>
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        );
+      })}
+      {/* <Card>
+        <Card.Content>
+          <Card.Header>Steve Sanders</Card.Header>
+          <Card.Meta>Friends of Elliot</Card.Meta>
+          <Card.Description>
+            Steve wants to add you to the group <strong>best friends</strong>
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <div className="ui two buttons">
+            <Button basic color="green">
+              Approve
+            </Button>
+            <Button basic color="red">
+              Decline
+            </Button>
+          </div>
+        </Card.Content>
+      </Card> */}
+    </div>
   );
 };
 export default UserTable;
