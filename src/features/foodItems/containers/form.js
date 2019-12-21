433d03c1c80ea54ca6e3525d80ede591c0133062
anees <anees@syook.com>
2019-12-21 17:00:39 +0530

    crud with graphql

--- src/features/foodItems/containers/form.js
+++ src/features/foodItems/containers/form.js
@@ -1,69 +1,132 @@
-import React from "react";
-import { Form, Button } from "semantic-ui-react";
+import React from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
+
+const CREATE_FOOD_ITEM_MUTATION = gql`
+	mutation createFoodItemMutation($name: String!, $type: String!) {
+		createFoodItem(name: $name, type: $type) {
+			_id
+			name
+			type
+		}
+	}
+`;
+
+const UPDATE_FOOD_ITEM_MUTATION = gql`
+	mutation updateFoodItemMutation($id: String!, $name: String!, $type: String!) {
+		updateFoodItem(id: $id, name: $name, type: $type) {
+			_id
+			name
+			type
+		}
+	}
+`;
 
 class FoodItemsForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      options: [
-        { text: "Breakfast", value: "breakfast" },
-        { text: "Lunch", value: "lunch" }
-      ],
-      selectedType: "breakfast"
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			foodItem: {
+				name: '',
+				type: 'breakfast',
+			},
+			options: [
+				{ text: 'Breakfast', value: 'breakfast' },
+				{ text: 'Lunch', value: 'lunch' },
+			],
+		};
+	}
 
-  handleChange = e => {
-    if (e.target) this.setState({ [e.target.name]: e.target.value });
-    else this.setState({ selectedType: e });
-  };
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log(this.state);
-    this.setState({ name: "" });
-  };
+	componentDidUpdate(prevProps) {
+		if (this.props.foodItem && this.props.foodItem !== prevProps.foodItem) {
+			this.setState({
+				foodItem: this.props.foodItem,
+			});
+		}
+	}
 
-  render() {
-    return (
-      <div>
-        <Form>
-          <Form.Field>
-            {/* <Form.Select
+	handleChange = e => {
+		if (e.target)
+			this.setState({
+				foodItem: {
+					...this.state.foodItem,
+					[e.target.name]: e.target.value,
+				},
+			});
+		else this.setState({ type: e });
+	};
+
+	render() {
+		const { name, type } = this.state.foodItem;
+		console.log(name, type);
+		return (
+			<div>
+				<Form>
+					<Form.Field>
+						{/* <Form.Select
               fluid
               label="Type"
-              name="selectedType"
+              name="type"
               options={this.state.options}
               placeholder="Type"
               onChange={(e, { value }) => this.handleChange(value)}
             /> */}
-            <Form.Dropdown
-              fluid
-              name="defaultRice"
-              defaultValue={"breakfast"}
-              placeholder="Rice"
-              options={this.state.options}
-              onChange={(e, { value }) => this.handleChange(value)}
-            />
-          </Form.Field>
-          <Form.Field>
-            <label> Name</label>
-            <input
-              name="name"
-              placeholder="Item Name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Button onClick={this.props.toggle}>Cancel</Button>
-          <Button type="submit" onClick={this.handleSubmit}>
-            Submit
-          </Button>
-        </Form>
-      </div>
-    );
-  }
+						<Form.Dropdown
+							fluid
+							name="defaultRice"
+							defaultValue={'breakfast'}
+							placeholder="Rice"
+							options={this.state.options}
+							onChange={(e, { value }) => this.handleChange(value)}
+						/>
+					</Form.Field>
+					<Form.Field>
+						<label> Name</label>
+						<input
+							name="name"
+							placeholder="Item Name"
+							type="text"
+							value={name}
+							onChange={this.handleChange}
+						/>
+					</Form.Field>
+					<Button onClick={this.props.toggle}>Cancel</Button>
+					{!!this.props.foodItem ? (
+						<Mutation
+							mutation={UPDATE_FOOD_ITEM_MUTATION}
+							variables={{ id: this.props.foodItem._id, name, type: type }}
+							onCompleted={this.props.toggle}
+							update={(store, { data: { updateFoodItem } }) =>
+								this.props.updateFoodItem(store, updateFoodItem)
+							}
+						>
+							{createFoodItemMutation => (
+								<Button type="submit" onClick={createFoodItemMutation}>
+									Submit
+								</Button>
+							)}
+						</Mutation>
+					) : (
+						<Mutation
+							mutation={CREATE_FOOD_ITEM_MUTATION}
+							variables={{ name, type: type }}
+							onCompleted={this.props.toggle}
+							update={(store, { data: { createFoodItem } }) =>
+								this.props.addFoodItem(store, createFoodItem)
+							}
+						>
+							{createFoodItemMutation => (
+								<Button type="submit" onClick={createFoodItemMutation}>
+									Submit
+								</Button>
+							)}
+						</Mutation>
+					)}
+				</Form>
+			</div>
+		);
+	}
 }
 
 export default FoodItemsForm;
--- src/features/foodItems/containers/index.js
+++ src/features/foodItems/containers/index.js
@@ -1,44 +1,163 @@
-import React from "react";
-import FoodItemsForm from "./form";
-import FoodItemTable from "./table";
-import { Button } from "semantic-ui-react";
+import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { withApollo } from 'react-apollo';
 
-export default class FoodItems extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [
-        { name: "sandwich", type: "breakfast" },
-        { name: "paratha", type: "breakfast" },
-        { name: "sandwich", type: "breakfast" },
-        { name: "paratha", type: "breakfast" },
-        { name: "sandwich", type: "breakfast" },
-        { name: "paratha", type: "breakfast" },
-        { name: "sandwich", type: "breakfast" },
-        { name: "paratha", type: "breakfast" }
-      ],
-      isAddItem: false
-    };
-  }
-  toggle = () => {
-    this.setState({ isAddItem: !this.state.isAddItem });
-  };
+import FoodItemsForm from './form';
+import FoodItemTable from './table';
+import { Button } from 'semantic-ui-react';
+import { Mutation } from 'react-apollo';
 
-  componentDidMount(){
-      //api call
-  }
+const FOOD_ITEM_QUERY = gql`
+	{
+		foodItems {
+			_id
+			name
+			type
+		}
+	}
+`;
 
-  render() {
-    return (
-      <div style={{ marginTop: "80px", width: "90vw" }}>
-        {this.state.isAddItem ? (
-          <FoodItemsForm toggle={this.toggle} />
-        ) : (
-          <>
-            <FoodItemTable currentData={this.state.data} toggle={this.toggle}/>
-          </>
-        )}
-      </div>
-    );
-  }
+const GET_FOOD_ITEM = gql`
+	query GetFoodItemQuery($id: String!) {
+		foodItem(id: $id) {
+			_id
+			name
+			type
+		}
+	}
+`;
+
+const DElETE_FOOD_ITEM = gql`
+	mutation deleteFoodItemMutation($id: String!) {
+		deleteFoodItem(id: $id) {
+			_id
+		}
+	}
+`;
+
+class FoodItems extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{ name: 'sandwich', type: 'breakfast' },
+				{ name: 'paratha', type: 'breakfast' },
+				{ name: 'sandwich', type: 'breakfast' },
+				{ name: 'paratha', type: 'breakfast' },
+				{ name: 'sandwich', type: 'breakfast' },
+				{ name: 'paratha', type: 'breakfast' },
+				{ name: 'sandwich', type: 'breakfast' },
+				{ name: 'paratha', type: 'breakfast' },
+			],
+			isAddItem: false,
+			foodItem: null,
+		};
+	}
+
+	componentDidMount() {
+		if (this.props.match.params.id) {
+			this.fetchFoodItem(this.props.match.params.id);
+		}
+	}
+
+	componentDidUpdate(prevProps) {
+		if (this.props.match.params.id !== prevProps.match.params.id) {
+			this.fetchFoodItem(this.props.match.params.id);
+		}
+	}
+
+	fetchFoodItem = async id => {
+		const response = await this.props.client.query({
+			query: GET_FOOD_ITEM,
+			variables: { id },
+		});
+		const { foodItem } = response.data;
+		this.setState({ foodItem });
+	};
+
+	toggle = () => {
+		this.setState({ isAddItem: !this.state.isAddItem });
+	};
+
+	addFoodItem = (store, foodItem) => {
+		const data = store.readQuery({ query: FOOD_ITEM_QUERY });
+		data.foodItems.unshift(foodItem);
+		store.writeQuery({
+			query: FOOD_ITEM_QUERY,
+			data,
+		});
+	};
+
+	updateFoodItem = (store, foodItem) => {
+		const data = store.readQuery({ query: FOOD_ITEM_QUERY });
+		const index = data.foodItems.findIndex(item => item._id === foodItem._id);
+		data[index] = foodItem;
+		store.writeQuery({
+			query: FOOD_ITEM_QUERY,
+			data,
+		});
+	};
+
+	editFoodItem = id => {
+		this.props.history.push(`/fooditems/${id}`);
+	};
+
+	deleteFoodItem = (store, foodItem) => {
+		const data = store.readQuery({ query: FOOD_ITEM_QUERY });
+		const index = data.foodItems.findIndex(item => item._id === foodItem._id);
+		const newData = [...data.foodItems];
+		newData.splice(index, 1);
+		store.writeQuery({
+			query: FOOD_ITEM_QUERY,
+			data: { ...data, foodItems: newData },
+		});
+	};
+
+	render() {
+		return (
+			<div style={{ marginTop: '80px', width: '90vw' }}>
+				<FoodItemsForm toggle={this.toggle} addFoodItem={this.addFoodItem} foodItem={this.state.foodItem} />
+
+				<>
+					<FoodItemTable currentData={this.state.data} toggle={this.toggle} />
+					<Query query={FOOD_ITEM_QUERY}>
+						{({ loading, error, data }) => {
+							if (loading) return <div>Fetching</div>;
+							if (error) return <div>Error</div>;
+
+							const foodItems = data.foodItems;
+							return (
+								<div>
+									{foodItems.map(foodItem => (
+										<>
+											<p>
+												{foodItem.name} - {foodItem.type} -{' '}
+												<p onClick={() => this.editFoodItem(foodItem._id)}>edit</p>
+												<Mutation
+													mutation={DElETE_FOOD_ITEM}
+													variables={{ id: foodItem._id }}
+													update={(store, { data: { deleteFoodItem } }) =>
+														this.deleteFoodItem(store, deleteFoodItem)
+													}
+												>
+													{deleteFoodItemMutation => (
+														<Button type="submit" onClick={deleteFoodItemMutation}>
+															delete
+														</Button>
+													)}
+												</Mutation>
+											</p>
+										</>
+									))}
+								</div>
+							);
+						}}
+					</Query>
+				</>
+			</div>
+		);
+	}
 }
+
+export default withApollo(FoodItems);
